3a2491f4fb6f576fe1607956bd016b50e04a7ae9
ladihzey <ladihzey@proton.me>
2023-06-29 21:40:12 +0200

    feat: query user for search term using 'inquirer'

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import playwright from "playwright";
+import inquirer from "inquirer";
+
+const { searchTerm } = await inquirer.prompt([{
+    type: "input",
+    name: "searchTerm",
+    message: "What are you looking for?",
+}]);
+
+console.log(searchTerm);
 
 const browser = await playwright["chromium"].launch({
     headless: false,
@@ -6,5 +15,6 @@ const browser = await playwright["chromium"].launch({
 const context = await browser.newContext();
 const page = await context.newPage();
 await page.goto("https://amazon.com");
-await page.waitForTimeout(1000);
+await page.waitForLoadState('networkidle');
+await page.waitForTimeout(2000);
 await browser.close();
